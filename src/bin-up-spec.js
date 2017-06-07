2e137c49090d712f1972908ac7f41196e9aaf328
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-07 17:03:12 -0400

    initial code

---
+++ src/bin-up-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* global describe, it */
+const binUp = require('.')
+
+describe('bin-up', () => {
+  it('write this test', () => {
+    console.assert(binUp, 'should export something')
+  })
+})
