1b2a02b3182b4d27567ff82d858b4e9639b1871f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-25 09:32:34 -0400

    chore: add e2e test

--- src/bin-up-spec.js
+++ src/bin-up-spec.js
@@ -1,10 +1,23 @@
 'use strict'
 
-/* global describe, it */
+/* eslint-env mocha */
 const binUp = require('.')
+const la = require('lazy-ass')
+const execa = require('execa-wrap')
+const join = require('path').join
 
 describe('bin-up', () => {
   it('write this test', () => {
     console.assert(binUp, 'should export something')
+  })
+
+  it('finds standard', () => {
+    const nested = join(__dirname, '..', 'test', 'nested')
+    return execa('npm', ['run', 'find:standard'], {
+      cwd: nested
+    }).then(result => {
+      la(result.includes('code: 0'), 'non-zero code', result)
+      la(result.includes('Found '), 'shows found', result)
+    })
   })
 })
